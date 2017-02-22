bd69e48823ed2178ac4c0beac3414269de68a4ea
Hashir Baig <hashirbaig@gmail.com>
2017-02-23 02:31:50 +0500

    ManageCategories - Added functionality to view all Categories and Delete any Category

--- myAmazon-v1/AdminPanel/ManageCategories.aspx
+++ myAmazon-v1/AdminPanel/ManageCategories.aspx
@@ -4,13 +4,40 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <link href="../Content/bootstrap.css" rel="stylesheet" />
+    <title>Manage Categories</title>
 </head>
 <body>
-    <form id="form1" runat="server">
-    <div>
-    
+    <div class="container">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <asp:ListView ID="categoriesListView" runat="server" ItemPlaceholderID="categoryPlaceHolder">
+                <LayoutTemplate>
+                    <h3>Categories List</h3>
+                    <table class="table table-hover">
+                        <tr>
+                            <th>ID</th><th>Name</th><th>Description</th><th>Edit</th><th>Delete</th>
+                        </tr>
+                        <asp:PlaceHolder runat="server" ID="categoryPlaceHolder"></asp:PlaceHolder>
+                    </table>
+                </LayoutTemplate>
+                <ItemTemplate>
+                    <form method="post">
+                        <input type="hidden" name="id" value="<%#Eval("id")%>"/>
+                        <tr>
+                            <td><%#Eval("id")%></td>
+                            <td><%#Eval("Name")%></td>
+                            <td><%#Eval("Desc")%></td>
+                            <td><input type="submit" class="btn btn-warning" name="Action" value="Edit"></td>
+                            <td><input type="submit" class="btn btn-danger" name="Action" value="Delete"></td>
+                        </tr>
+                    </form>
+                </ItemTemplate>
+            </asp:ListView>
+        </div>
+        <div class="col-md-1">
+            <asp:Label ID="log_manage_cat" runat="server" Text=""></asp:Label>
+        </div>
     </div>
-    </form>
 </body>
 </html>
--- myAmazon-v1/AdminPanel/ManageCategories.aspx.cs
+++ myAmazon-v1/AdminPanel/ManageCategories.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,55 @@ namespace myAmazon_v1.AdminPanel
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!this.IsPostBack)
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-HO7NA1P;Initial Catalog=myAmazon;User ID=sa;Password=root");
+                string cmd = "SELECT [id], [Name], [Desc] FROM Category";
+                SqlCommand sqlCmd = new SqlCommand(cmd, conn);
+                DataSet ds = new DataSet();
 
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(sqlCmd);
+                    adapter.Fill(ds);
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    log_manage_cat.Text += ex.ToString();
+                    conn.Close();
+                }
+
+                categoriesListView.DataSource = ds;
+                categoriesListView.DataBind();
+            }
+
+            if (Request.HttpMethod.ToString() == "POST") {
+                switch (HttpContext.Current.Request.Form["Action"]) {
+                    case "Delete": {
+                        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-HO7NA1P;Initial Catalog=myAmazon;User ID=sa;Password=root");
+                        string cmd = "DELETE FROM Category WHERE id=@catId";
+                        SqlCommand sqlCmd = new SqlCommand(cmd, conn);
+                        sqlCmd.Parameters.AddWithValue("catId", HttpContext.Current.Request["id"]);
+
+                        try
+                        {
+                            conn.Open();
+                            sqlCmd.ExecuteNonQuery();
+                            conn.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            log_manage_cat.Text += ex.ToString();
+                            conn.Close();
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
