063c21b6851f572ac5cd638dde6402d10c8cd0b3
Hashir Baig <hashirbaig@gmail.com>
2017-02-24 00:35:11 +0500

    Added ManageBrands: List all Brands and can delete any of them.

---
+++ myAmazon-v1/AdminPanel/ManageBrands.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ManageBrands.aspx.cs" Inherits="myAmazon_v1.AdminPanel.ManageBrands" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="../Content/bootstrap.css" rel="stylesheet" />
+    <title>Manage Brands</title>
+</head>
+<body>
+    <div class="container">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <asp:ListView ID="brandListView" runat="server" ItemPlaceholderID="brandPlaceHolder">
+                <LayoutTemplate>
+                    <h3>Brands List</h3>
+                    <table class="table table-hover">
+                        <tr>
+                            <th>ID</th><th>Name</th><th>Category</th><th>Description</th><th>Edit</th><th>Delete</th>
+                        </tr>
+                        <asp:PlaceHolder runat="server" ID="brandPlaceHolder"></asp:PlaceHolder>
+                    </table>
+                </LayoutTemplate>
+                <ItemTemplate>
+                    <form method="post">
+                        <input type="hidden" name="id" value="<%#Eval("id")%>"/>
+                        <tr>
+                            <td><%#Eval("id")%></td>
+                            <td><%#Eval("Name")%></td>
+                            <td><%#Eval("CategoryId")%></td>
+                            <td><%#Eval("Desc")%></td>
+                            <td><input type="submit" class="btn btn-warning" name="Action" value="Edit"></td>
+                            <td><input type="submit" class="btn btn-danger" name="Action" value="Delete"></td>
+                        </tr>
+                    </form>
+                </ItemTemplate>
+            </asp:ListView>
+        </div>
+        <div class="col-md-1">
+            <asp:Label ID="log_manage_brand" runat="server" Text=""></asp:Label>
+        </div>
+    </div>
+</body>
+</html>
---
+++ myAmazon-v1/AdminPanel/ManageBrands.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace myAmazon_v1.AdminPanel
+{
+    public partial class ManageBrands : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-HO7NA1P;Initial Catalog=myAmazon;User ID=sa;Password=root");
+                string cmd = "SELECT [id], [Name], [Desc], [CategoryId] FROM Brand";
+                SqlCommand sqlCmd = new SqlCommand(cmd, conn);
+                DataSet ds = new DataSet();
+
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(sqlCmd);
+                    adapter.Fill(ds);
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    log_manage_brand.Text += ex.ToString();
+                    conn.Close();
+                }
+
+                brandListView.DataSource = ds;
+                brandListView.DataBind();
+            }
+
+            if (Request.HttpMethod.ToString() == "POST")
+            {
+                switch (HttpContext.Current.Request.Form["Action"])
+                {
+                    case "Delete":
+                        {
+                            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-HO7NA1P;Initial Catalog=myAmazon;User ID=sa;Password=root");
+                            string cmd = "DELETE FROM Brand WHERE id=@brandId";
+                            SqlCommand sqlCmd = new SqlCommand(cmd, conn);
+                            sqlCmd.Parameters.AddWithValue("brandId", HttpContext.Current.Request["id"]);
+
+                            try
+                            {
+                                conn.Open();
+                                sqlCmd.ExecuteNonQuery();
+                                conn.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                log_manage_brand.Text += ex.ToString();
+                                conn.Close();
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
