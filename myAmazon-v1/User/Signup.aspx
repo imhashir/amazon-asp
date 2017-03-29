4aecf2aac1716286ed1cbdc1dc30f305f74c8a6a
Hashir Baig <hashirbaig@gmail.com>
2017-03-29 15:38:28 +0500

    Added SignUp Functionality

--- myAmazon-v1/User/Signup.aspx
+++ myAmazon-v1/User/Signup.aspx
@@ -4,5 +4,27 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
-
+    <div class="container">
+        <div class="jumbotron">        
+            <div class="row">
+                <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                        <h3><b><asp:Label ID="id_page_title" runat="server" Text="Register a New Account"></asp:Label></b></h3>
+                        First Name:  <asp:TextBox ID="id_user_fname" runat="server" CssClass="form-control"></asp:TextBox><br />
+                        Last Name:  <asp:TextBox ID="id_user_lname" runat="server" CssClass="form-control"></asp:TextBox><br />
+                        Username:  <asp:TextBox ID="id_username" runat="server" CssClass="form-control"></asp:TextBox><br />
+                        Email:  <asp:TextBox ID="id_email" runat="server" CssClass="form-control"></asp:TextBox><br />
+                        Password:  <asp:TextBox ID="id_password" runat="server" CssClass="form-control"></asp:TextBox><br />
+                        Contact Number:  <asp:TextBox ID="id_cnumber" runat="server" CssClass="form-control"></asp:TextBox><br />
+                        <div>
+                            <asp:Image ID="id_cimage" Width="200px" ImageUrl="~/ProductsData/Images/Default.jpg" runat="server" />
+                            <asp:FileUpload ID="id_image_uploader" runat="server" onchange="if (confirm('Upload ' + this.value + '?')) this.form.submit();"/>
+                        </div>
+                        <asp:Button ID="id_submit_signup" runat="server" CssClass="btn btn-primary" Text="Signup" OnClick="id_submit_signup_Click"/><br />
+                        <asp:Label ID="id_log_signup" runat="server" CssClass="alert-warning" Text=" "></asp:Label>
+                    </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- myAmazon-v1/User/Signup.aspx.cs
+++ myAmazon-v1/User/Signup.aspx.cs
@@ -4,14 +4,65 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
 namespace myAmazon_v1.User
 {
     public partial class Signup : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(id_image_uploader.HasFile)
+            {
+                onImageUploaded();
+            }
+        }
 
+        protected void id_submit_signup_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager
+                        .ConnectionStrings["myAmazonConnectionString"].ConnectionString);
+            SqlCommand sqlCmd = new SqlCommand("SignUp", conn);
+            string imagePath = null;
+            
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+
+            sqlCmd.Parameters.AddWithValue("@username", id_username.Text);
+            sqlCmd.Parameters.AddWithValue("@firstName", id_user_fname.Text);
+            sqlCmd.Parameters.AddWithValue("@lastName", id_user_lname.Text);
+            sqlCmd.Parameters.AddWithValue("@email", id_email.Text);
+            sqlCmd.Parameters.AddWithValue("@pass", id_password.Text);
+            sqlCmd.Parameters.AddWithValue("@number", id_cnumber.Text);
+            sqlCmd.Parameters.AddWithValue("@img", imagePath ?? (object)DBNull.Value);
+
+            if(File.Exists(Server.MapPath(@"~\UserData\Images\" + "_temp" + ".jpg")))
+            {
+                File.Copy(Server.MapPath(@"~\UserData\Images\" + "_temp" + ".jpg"), Server.MapPath(@"~\UserData\Images\" + id_username.Text + ".jpg"));
+                File.Delete(Server.MapPath(@"~\UserData\Images\" + "_temp" + ".jpg"));
+            }
+            SqlParameter outputFlag = sqlCmd.Parameters.Add("@flag", SqlDbType.Int);
+            outputFlag.Direction = ParameterDirection.Output;
+            int flag = 0;
+            try
+            {
+                conn.Open();
+                sqlCmd.ExecuteNonQuery();
+                flag = (int) sqlCmd.Parameters["@flag"].Value;
+                conn.Close();
+                id_log_signup.Text = "SignUp Successful!";
+            } catch (Exception ex)
+            {
+                id_log_signup.Text = ex.ToString();
+                conn.Close();
+            }
+        }
+
+        protected void onImageUploaded() {
+            string imagePath = @"~\UserData\Images\" + "_temp" + ".jpg";
+            id_image_uploader.SaveAs(Server.MapPath(imagePath));
+            id_cimage.ImageUrl = imagePath;
         }
     }
 }
